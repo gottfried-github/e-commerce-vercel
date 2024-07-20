6b3d0a1879dcfebd8aa677d69e0255922d829ba6
gottfried <gottfried.dev@gmail.com>
2024-07-20 23:46:27 +0300

    upd index and env examplke

--- api/index.js
+++ api/index.js
@@ -15,15 +15,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 function main(port) {
   /* ensure environment variables */
-  // database connection data
-  if (
-    !process.env.APP_DB_NAME ||
-    !process.env.APP_DB_USER ||
-    !process.env.APP_DB_PASS ||
-    !process.env.NET_NAME
-  )
-    throw new Error('all of the database connection parameters environment variables must be set')
-
   // express-session uses this
   if (!process.env.SESSION_SECRETS)
     throw new Error('SESSION_SECRETS environment variable must be set')
@@ -34,7 +25,7 @@ function main(port) {
     saveUninitialized: false,
     resave: false,
     store: SessionStorage.create({
-      mongoUrl: `mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`,
+      mongoUrl: process.env.MONGODB_URI,
       collectionName: 'sessions',
       touchAfter: 24 * 3600,
     }),
@@ -48,9 +39,7 @@ function main(port) {
   if (process.env.NODE_ENV === 'production') sessionOptions.cookie.secure = true
 
   /* connect to database */
-  const client = new MongoClient(
-    `mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`
-  )
+  const client = new MongoClient(process.env.MONGODB_URI)
   client.connect()
 
   /* initialize store and api */
@@ -72,41 +61,20 @@ function main(port) {
 
   // send index.html from anywhere: let front-end handle routing
   app.use(['/admin', '/admin/*'], (req, res, next) => {
-    res.sendFile(path.join(__dirname, './dist/front-end/admin.html'))
+    res.sendFile(path.join(__dirname, '../dist/front-end/admin.html'))
   })
 
-  app.use('/', express.static(path.join(__dirname, './dist/front-end')))
-  app.use('/', express.static(path.join(__dirname, './public')))
+  app.use('/', express.static(path.join(__dirname, '../dist/front-end')))
+  app.use('/', express.static(path.join(__dirname, '../public')))
 
   app.use('/*', (req, res, next) => {
-    res.sendFile(path.join(__dirname, './dist/front-end/visitor.html'))
+    res.sendFile(path.join(__dirname, '../dist/front-end/visitor.html'))
   })
 
   /* start server */
-  let server = null
-
-  if (process.env.NODE_ENV === 'production') {
-    if (!process.env.SSL_CERT_KEY_PATH || !process.env.SSL_CERT_PATH)
-      throw new Error(
-        'SSL_CERT_KEY_PATH and SSL_CERT_PATH environment variables must be set if NODE_ENV is production'
-      )
-
-    server = https.createServer(
-      {
-        key: fs.readFileSync(path.join(__dirname, process.env.SSL_CERT_KEY_PATH)),
-        cert: fs.readFileSync(path.join(__dirname, process.env.SSL_CERT_PATH)),
-      },
-      app
-    )
-
-    server.listen(port, () => {
-      console.log(`secure server listening on port ${port}`)
-    })
-  } else {
-    server = app.listen(port, () => {
-      console.log(`listening on port ${port}`)
-    })
-  }
+  const server = app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+  })
 
   return { app, server, api, store }
 }
